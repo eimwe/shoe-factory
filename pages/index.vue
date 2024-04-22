434bfd05610488ff359800637d55333d7fdeb1cc
eimwe <prokopenkoserge@inbox.ru>
2024-04-22 16:50:23 +0500

    Made index page template as a default route

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+useHead({
+  title: "Главный экран",
+});
+</script>
+
+<template>
+  <div>
+    <h1 class="capitalize">Автоматизированное рабочее место технолога</h1>
+  </div>
+</template>
