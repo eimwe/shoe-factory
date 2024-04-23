4a368dc69da6133d8a0cb6d1896b1a1b1dd03561
eimwe <prokopenkoserge@inbox.ru>
2024-04-23 14:40:56 +0500

    Updated default layout 1. Checked whether user is looged in 2. Render index vs login page as a result of the above check-up.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,27 @@
+<script setup lang="ts">
+import { useIsLoadingStore } from "~/store/loading.store";
+import { useAuthStore } from "~/store/auth.store";
+
+const isLoadingStore = useIsLoadingStore();
+const authStore = useAuthStore();
+const router = useRouter();
+
+onMounted(async () => {
+  try {
+    const user = await account.get();
+    if (user) authStore.set(user);
+  } catch (error) {
+    router.push("/login");
+  } finally {
+    isLoadingStore.set(false);
+  }
+});
+</script>
+
 <template>
-  <section class="grid grid-cols-[1fr_6fr]">
-    <LayoutSidebar />
+  <LayoutLoader v-if="isLoadingStore.isLoading" />
+  <section v-else :class="authStore.isAuth ? 'grid grid-cols-[1fr_6fr]' : ''">
+    <LayoutSidebar v-if="authStore.isAuth" />
     <div>
       <slot />
     </div>
