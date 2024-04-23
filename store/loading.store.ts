e85d2ba4a13e61ecc2097346b51144d9268e852a
eimwe <prokopenkoserge@inbox.ru>
2024-04-23 12:12:24 +0500

    Defined global loader state manager

---
+++ store/loading.store.ts
@@ -0,0 +1,12 @@
+import { defineStore } from "pinia";
+
+export const useIsLoadingStore = defineStore("isLoading", {
+  state: () => ({
+    isLoading: true,
+  }),
+  actions: {
+    set(data: boolean) {
+      this.$patch({ isLoading: data });
+    },
+  },
+});
