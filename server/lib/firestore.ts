24d00cd7b33ea70aab5dffe416b4bdf349196682
eimwe <prokopenkoserge@inbox.ru>
2024-04-29 04:07:48 +0500

    Added query to get single doc

--- server/lib/firestore.ts
+++ server/lib/firestore.ts
@@ -79,6 +79,65 @@ export const queryByCollection = async (
   return docs;
 };
 
+export const queryById = async (
+  col: string,
+  docId: string,
+): Promise<DocumentData & { id: string }> => {
+  const docRef = doc(collection(FIRESTORE_DB, col), docId);
+  const docSnapshot = await getDoc(docRef);
+
+  if (!docSnapshot.exists()) {
+    throw new Error(
+      `Document with ID ${docId} does not exist in collection ${col}.`,
+    );
+  }
+
+  const docData = docSnapshot.data();
+
+  // Check if any fields in the document are nested documents
+  const nestedFields: string[] = Object.keys(docData).filter(
+    (key) =>
+      typeof docData[key] === "object" &&
+      !(docData[key] instanceof DocumentReference),
+  );
+
+  // If there are nested documents, retrieve the referenced documents
+  await Promise.all(
+    nestedFields.map(async (field) => {
+      if (Array.isArray(docData[field])) {
+        // If the field is an array of nested documents, loop through each nested document and fetch its data
+        docData[field] = await Promise.all(
+          docData[field].map(async (nestedDocRef: any) => {
+            if (nestedDocRef instanceof DocumentReference) {
+              // If the nested reference is a DocumentReference, fetch its data
+              const nestedDocSnapshot = await getDoc(nestedDocRef);
+              return nestedDocSnapshot.exists()
+                ? {
+                    ...nestedDocSnapshot.data(),
+                    id: nestedDocSnapshot.id,
+                  }
+                : null;
+            }
+          }),
+        );
+      } else {
+        await Promise.all(
+          Object.entries(docData).map(async ([key, value]) => {
+            if (value instanceof DocumentReference) {
+              const nestedDocSnapshot = await getDoc(value);
+              docData[key] = nestedDocSnapshot.exists()
+                ? { ...nestedDocSnapshot.data(), id: nestedDocSnapshot.id }
+                : null;
+            }
+          }),
+        );
+      }
+    }),
+  );
+
+  return { ...docData, id: docSnapshot.id };
+};
+
 export const set = async (col: string, document: Object) => {
   await setDoc(doc(collection(FIRESTORE_DB, col)), document);
 };
